e9e513c3c23e3bf83756372cea38d1aae6019a75
Ian Gilham <ian.gilham@gmail.com>
2012-11-28 23:11:00 +0000

    reordered imports

--- src/main/java/com/iangilham/properties/Properties.java
+++ src/main/java/com/iangilham/properties/Properties.java
@@ -1,11 +1,11 @@
 package com.iangilham.properties;
 
-import java.util.HashMap;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.HashMap;
 
 /**
  * The Properties class represents a persistent set of properties in the key=value format.
@@ -19,7 +19,7 @@ import java.io.Writer;
  * 
  * One of the main weaknesses of standard Java Properties implementation is that it 
  * defaults to ISO-8859-1 as its output encoding. This implementation defaults to UTF-8,
- * removing the burden of using unicode escape characters for common glyphs in many languages.
+ * removing the burden of using Unicode escape characters for common glyphs in many languages.
  * 
  * @see	java.util.Properties
  */
@@ -34,40 +34,40 @@ public class Properties extends HashMap<String, String> {
 	
 	/**
 	 * Construct a new instance of Properties using the contents of an existing instance.
-	 * @param defaults The pre-existing instance from which to take default values during
+	 * @param   defaults    The pre-existing instance from which to take default values during
 	 * construction.
 	 */
-	public Properties(Properties defaults) {
+	public Properties(final Properties defaults) {
 		super(defaults);
 	}
 	
 	/**
 	 * Load properties from an InputStream.
-	 * @param	inStream	The InputStream from which to read properties.
+	 * @param   inStream    The InputStream from which to read properties.
 	 */
-	public void load(InputStream inStream) throws IOException {
+	public void load(final InputStream inStream) throws IOException {
 	}
 	
 	/**
 	 * Load properties from a Reader.
-	 * @param	reader	The Reader from which to read properties.
+	 * @param   reader  The Reader from which to read properties.
 	 */
-	public void load(Reader reader) throws IOException {
+	public void load(final Reader reader) throws IOException {
 	}
 	
 	/**
 	 * Store properties by writing to an OutputStream.
-	 * @param	outStream	The stream to write to.
-	 * @param	comments	A comment string to prepend to the output.
+	 * @param   outStream   The stream to write to.
+	 * @param   comments    A comment string to prepend to the output.
 	 */
-	public void store(OutputStream outStream, String comments) throws IOException {
+	public void store(final OutputStream outStream, final String comments) throws IOException {
 	}
 	
 	/**
 	 * Store properties by writing to a Writer.
-	 * @param	writer	The stream to write to.
-	 * @param	comments	A comment string to prepend to the output.
+	 * @param   writer      The stream to write to.
+	 * @param   comments    A comment string to prepend to the output.
 	 */
-	public void store(Writer writer, String comments) throws IOException {
+	public void store(final Writer writer, final String comments) throws IOException {
 	}
 }
